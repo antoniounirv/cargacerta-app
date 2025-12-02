6a9d1b1ea0fa6c84a81e14f9c03838334d7ff630
ANTONIO TARGINO NETO <antonio.t.neto@academico.unirv.edu.br>
2025-12-02 00:49:26 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // Popula o firebaseConfig a partir das variáveis de ambiente para uso no cliente
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
+  apiKey: "AIzaSyBZ2n6u1znFT3_osHgKvnKYwvCCLS2vJZw",
+  authDomain: "studio-4936707644-3984e.firebaseapp.com",
+  projectId: "studio-4936707644-3984e",
+  storageBucket: "",
+  messagingSenderId: "350134173132",
+  appId: "1:350134173132:web:fdda59039368e24d55b169",
+  measurementId: ""
 };
